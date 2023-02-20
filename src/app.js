d29e0cc55d29b7044ca40d25a0c87bb46b90bdca
devrobson93 <robson.dev93@gmail.com>
2023-02-20 02:13:33 +0000

    Domain

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const pronoum = ["the", "our"];
+  const adjt = ["great", "big"];
+  const subjet = ["jogger", "racoon"];
+  const extension = [".com", ".es", ".net"];
+  getGenerator(pronoum, adjt, subjet, extension);
+};
+const getGenerator = (pronoum, adjt, subjet, extension) => {
+  for (let i = 0; i < pronoum.length; i++) {
+    for (let a = 0; a < adjt.length; a++) {
+      for (let b = 0; b < subjet.length; b++) {
+        for (let c = 0; c < extension.length; c++) {
+          console.log(`${pronoum[i]}${adjt[a]}${subjet[b]}${extension[c]}`);
+        }
+      }
+    }
+  }
 };
